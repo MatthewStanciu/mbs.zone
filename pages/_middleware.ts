071f475675a0008bd83a0ea6a5178f16c33f009a
Matthew Stanciu <mattbstanciu@gmail.com>
2021-11-16 14:13:07 -0500

    Use AirtablePlusPlus

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { AirtablePlusPlus, AirtablePlusPlusRecord } from 'airtable-plusplus'
 
 const fallback = 'https://matthewstanciu.me'
-const airtable = {
+const airtable = new AirtablePlusPlus({
+  apiKey: process.env.AIRTABLE_API_KEY,
   baseId: 'appwZU7mXFxKLVXs4',
-  tableName: 'ABLS',
-  apiKey: process.env.AIRTABLE_API_KEY
-}
+  tableName: 'ABLS'
+})
 
 export default async (req: NextRequest) => {
   const slug = req.nextUrl.pathname.replace('/', '')
@@ -18,15 +19,19 @@ export default async (req: NextRequest) => {
 }
 
 const findDestination = async (slug: string): Promise<string | null> => {
-  const records = await fetch(
-    `https://api.airtable.com/v0/${airtable.baseId}/${airtable.tableName}`,
-    { headers: { Authorization: `Bearer ${airtable.apiKey}` } }
-  )
-    .then(r => r.json())
-    .then(json => json.records)
-    .then((records: Array<{ fields: { slug: string; destination: string } }>) =>
-      records.map((record) => record.fields)
-    )
+  // const records = await fetch(
+  //   `https://api.airtable.com/v0/${airtable.baseId}/${airtable.tableName}`,
+  //   { headers: { Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}` } }
+  // )
+  //   .then(r => r.json())
+  //   .then(json => json.records)
+  //   .then((records: Array<{ fields: { slug: string; destination: string } }>) =>
+  //     records.map((record) => record.fields)
+  //   )
 
-  return records.find((record) => record.slug === slug.toLowerCase())?.destination
+  const records = (await airtable.read({
+    filterByFormula: `{slug} = ${slug}`
+  }))[0] as unknown as AirtablePlusPlusRecord<{ slug: string, destination: string, visits: number }>[]
+
+  return records.find((record) => record.fields.slug === slug.toLowerCase())?.fields.destination
 }
